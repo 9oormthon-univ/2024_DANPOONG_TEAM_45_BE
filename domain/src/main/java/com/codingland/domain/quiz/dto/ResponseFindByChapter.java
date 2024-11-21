6f5e9ca7c1db98d7a33ccdd2b5b85719fde8d4c6
NARUBROWN <codingshark@inu.ac.kr>
2024-11-21 18:22:47 +0900

    [Feat] Answer - Question - Chapter 관련 작업 수행

--- domain/src/main/java/com/codingland/domain/quiz/dto/ResponseFindByChapter.java
+++ domain/src/main/java/com/codingland/domain/quiz/dto/ResponseFindByChapter.java
@@ -1,12 +1,10 @@
 package com.codingland.domain.quiz.dto;
 
-import com.codingland.domain.quiz.common.QuizTypeEnum;
 import lombok.Builder;
 
 @Builder
 public record ResponseFindByChapter(
         Long quizId,
-        QuizTypeEnum type,
         String title,
         int level,
         boolean isCleared
